1d3fbe52489483cc6141d7bba3b751767210aa2c
joshua_Adams <joshuatadams@email.arizona.edu>
2015-10-22 13:10:03 -0700

    added second line in array per specs

--- src/view/PaintGUI.java
+++ src/view/PaintGUI.java
@@ -16,16 +16,17 @@ import model.MyOval;
 import model.MyRectangle;
 import model.PaintObject;
 
-public class PaintGUI extends JFrame{
-	
-	public static void main(String[] args){
+public class PaintGUI extends JFrame {
+
+	public static void main(String[] args) {
 		PaintGUI paintGUI = new PaintGUI();
 		paintGUI.setVisible(true);
 	}
-	public PaintGUI(){
+
+	public PaintGUI() {
 		MyLine theLine = new MyLine(new Point2D.Double(50, 50), new Point2D.Double(150, 150), Color.BLACK);
 		PaintPanel canvas = new PaintPanel(theLine);
-		
+
 		this.setSize(700, 700);
 		this.setLocation(500, 200);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,27 +35,18 @@ public class PaintGUI extends JFrame{
 		canvas.setLocation(0, 0);
 		canvas.setBackground(Color.BLACK);
 		this.add(canvas);
-		
+
 		List<PaintObject> shapes = new ArrayList<PaintObject>();
-		MyLine line = new MyLine(new Point2D.Double(24,42), new Point2D.Double(500, 500), Color.WHITE);
+		MyLine line = new MyLine(new Point2D.Double(24, 42), new Point2D.Double(500, 500), Color.WHITE);
 		shapes.add(line);
-		MyOval oval = new MyOval(new Point2D.Double(300,300), new Point2D.Double(200,200), Color.BLUE);
+		MyOval oval = new MyOval(new Point2D.Double(300, 300), new Point2D.Double(200, 200), Color.BLUE);
 		shapes.add(oval);
-		MyRectangle rectangle = new MyRectangle(new Point2D.Double(150,150), new Point2D.Double(25, 25), Color.YELLOW);
+		MyRectangle rectangle = new MyRectangle(new Point2D.Double(150, 150), new Point2D.Double(25, 25), Color.YELLOW);
 		shapes.add(rectangle);
-		
+		MyLine line2 = new MyLine(new Point2D.Double(204, 4), new Point2D.Double(500, 500), Color.WHITE);
+		shapes.add(line2);
 		canvas.drawShapes(shapes);
-		
-		
-		
-		
+
 	}
-	/*---------------------
-	//@Override
-	public void PaintComponent(Graphics g){
-		Graphics2D g2 = (Graphics2D) g;
-		
-	}
-	------------------------*/
 
 }
