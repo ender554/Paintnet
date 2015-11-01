020fe2d7e89eefa24014ee2318e3cb61a576f6b9
dspence84 <dspence@catmail.arizona.edu>
2015-11-01 15:42:31 -0700

    changed client to a smaller size and added a color chooser button

--- src/view/PaintGUI.java
+++ src/view/PaintGUI.java
@@ -10,12 +10,15 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -35,13 +38,14 @@ public class PaintGUI extends JFrame {
 	@SuppressWarnings("unused")
 	private Image image;
 	
-	
+	private JButton colorButton;
 	private JRadioButton ovalButton;
 	private JRadioButton lineButton;
 	private JRadioButton rectangleButton;
 	private JRadioButton imageButton;
 	private ButtonGroup bg;
 	private Color color;
+	private JColorChooser tcc;
 
 	// main method creates new PaintGUI and sets visible
 	public static void main(String[] args) {
@@ -71,9 +75,10 @@ public class PaintGUI extends JFrame {
 		JScrollPane pane = new JScrollPane();
 		PaintPanel canvas = new PaintPanel(this);
 		
-		this.setSize(1024, 768);
+		this.setSize(640, 480);
 		this.setLocation(300, 200);
 		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.color = Color.BLACK;
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.out.println("Closed");
@@ -95,6 +100,7 @@ public class PaintGUI extends JFrame {
 		this.add(pane, BorderLayout.CENTER);
 
 		//added buttons
+		colorButton = new JButton("Color");
 		ovalButton = new JRadioButton("Oval");
 		lineButton = new JRadioButton("Line");
 		lineButton.setSelected(true);
@@ -106,30 +112,26 @@ public class PaintGUI extends JFrame {
 		bg.add(lineButton);
 		bg.add(rectangleButton);
 		bg.add(imageButton);
-
+		
+		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(lineButton);
 		buttonPanel.add(rectangleButton);
 		buttonPanel.add(ovalButton);
 		buttonPanel.add(imageButton);
+		buttonPanel.add(colorButton);
 		buttonPanel.setLocation(getWidth() / 2, (int) (getHeight() * .78));
 		buttonPanel.setSize(400, 40);
 		this.add(buttonPanel, BorderLayout.NORTH);
-
-		//color chooser panel set up and function
-		final JColorChooser tcc = new JColorChooser();
-		tcc.getSelectionModel().addChangeListener(new ChangeListener() {
+		
+		colorButton.addActionListener(new ActionListener() {
 
 			@Override
-			public void stateChanged(ChangeEvent e) {
-				color = tcc.getColor();
-
+			public void actionPerformed(ActionEvent event) {				
+				color = JColorChooser.showDialog(null, "Select a color", color);
 			}
-		});
-
-		this.add(tcc, BorderLayout.SOUTH);
-
-		this.color = Color.BLACK;
+			
+		});		
 	}
 
 	public Class<?> getSelectedShape() {
